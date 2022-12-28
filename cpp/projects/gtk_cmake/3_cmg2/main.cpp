c30a5f2fdf7a352a1b243348df9e79650147102d
syryuauros <sy.ryu@aurostech.com>
2022-12-28 16:26:00 +0900

    update

---
+++ cpp/projects/gtk_cmake/3_cmg2/main.cpp
@@ -0,0 +1,28 @@
+#include <gtkmm.h>
+
+int main(int argc, char *argv[])
+{
+  auto app =
+    Gtk::Application::create(argc, argv,
+      "org.gtkmm.examples.base");
+
+  Gtk::Window window;
+  window.set_default_size(200, 200);
+
+  return app->run(window);
+}
+
+
+// #include <iostream>
+// #include "my.h"
+// #include "your.h"
+
+// int main()
+
+// {
+//   std::cout << "calling up myfunc : " << myfunc(3) << std::endl;
+//   printf("%d + 1 == %d\n", 3, myfunc(3));
+//   printf("This code is modified!.\n");
+//   printf("%d + 10 == %d\n", 3, your_func(3));
+//   return 0;
+// }
