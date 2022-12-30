854fc3159381bf540c77e87a4dc20838856deab8
syryuauros <sy.ryu@aurostech.com>
2022-12-30 11:23:54 +0900

    update

--- cpp/projects/gtkmm3/4_ToggleButton/main.cpp
+++ cpp/projects/gtkmm3/4_ToggleButton/main.cpp
@@ -1,12 +1,25 @@
-#include "examplewindow.h"
+// #include "examplewindow.h"
+// #include <gtkmm/application.h>
+
+// int main(int argc, char *argv[])
+// {
+//   Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+
+//   ExampleWindow window;
+
+//   //Shows the window and returns when it is closed.
+//   return app->run(window);
+// }
+
+#include "radiobuttons.h"
 #include <gtkmm/application.h>
 
 int main(int argc, char *argv[])
 {
   Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-  ExampleWindow window;
+  RadioButtons buttons;
 
   //Shows the window and returns when it is closed.
-  return app->run(window);
+  return app->run(buttons);
 }
