14fc3efbfc695a8c7980d0a4526d727bb1352b7e
syryuauros <sy.ryu@aurostech.com>
2023-01-06 16:27:16 +0900

    update

--- cpp/projects/plplot/plplot5_14.nix
+++ cpp/projects/plplot/plplot5_14.nix
@@ -30,9 +30,32 @@ stdenv.mkDerivation rec {
   };
 }
 
+  # manual install
+  # https://sourceforge.net/p/plplot/wiki/Linux/   -- Compile PLplot in linux
+  #
+  # $ tar -zxf plplot-5.14.0.tar.gz
+  # $ mkdir build_directory
+  # $ cd build_directory
+  # $ cmake -DCMAKE_SKIP_BUILD_RPATH=OFF -DCMAKE_INSTALL_PREFIX:PATH=../install_directory ../plplot-5.14.0y
+  # $ make
+  # $ make install
 
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/plplot/default.nix  -- nixpkgs#plplot/default.nix
 
   # https://plplot.sourceforge.net/docbook-manual/plplot-html-5.15.0/devices.html   --PLplot device kind & set device option
   # https://plplot.sourceforge.net/downloads.php   --PLplot official homepage
-  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/plplot/default.nix  -- nixpkgs#plplot/default.nix
-  #
+  # https://gist.github.com/tschoonj/c40bb9cca6719478f000   -- makefile,  error msg
+  # https://www.hardcopyworld.com/?p=2255   -- what is cairo?
+  # http://plplot.org/docbook-manual/plplot-html-5.15.0/plplot_configure_build_install.html   -- cmake, make, make install
+  # https://sourceforge.net/p/plplot/plplot/ci/master/tree/   -- source codes
+
+
+  # plplot/lib/pkgconfig/ doesn`t exist`
+  # >>  error msg ::: plplot-c++ not found, plplotd-c++ not found
+  # >>  solve ::: include pkgs.pkg-config in nativeBuildInputs
+
+  # plplot/include/plplot/plDevs.h  >> undefine extcairo
+  # >>  error msg ::: plplot must be built with the extcairo device!
+  # >>  solve1 ::: include pkgs.gtkmm3 in nativeBuildInputs  >>  define extcairo
+  # >>  error msg ::: Ctest make collision msg >> compile fail
+  # >>  solve2 ::: include "-DDEFAULT_NO_DEVICES=ON -DLPD_extcairo=ON"  >> collision error solve & compile success!
