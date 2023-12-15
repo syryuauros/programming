3e839070dad111e05e781afd5ee27b043e5b95a2
syryuauros <sy.ryu@aurostech.com>
2023-12-15 15:50:10 +0900

    [working-webUI-test4-FFTMulti] add export All Data as CSV

--- python3_10/projects/webUI/test4/html/script_FFTMulti_csvIO.js
+++ python3_10/projects/webUI/test4/html/script_FFTMulti_csvIO.js
@@ -57,6 +57,28 @@ function exportToCSV(keyStr) {
     URL.revokeObjectURL(url);
 }
 
+function exportAllDataToCSV() {
+    let datum = {
+        key1: iFFT_result,
+        key2: amp_result,
+        key3: phs_result,
+        key4: real_result,
+        key5: imag_result
+    };
+    for (let key in datum) {
+        Data0 = datum[key];
+        const csvFormat = Data0.map(row => row.join(',')).join('\n');
+        const blob = new Blob([csvFormat], { type: 'text/csv' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'data0.csv';
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+}
+
+
 function convertToAOA(csvData) {
     var rows = csvData.split("\n");
     var csvDataAOA = rows.map(row => row.split(/[\t,]/));
