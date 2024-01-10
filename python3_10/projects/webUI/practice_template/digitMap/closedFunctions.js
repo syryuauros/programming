00010c1af7fcf9c0b4755c50d7f263eebb2c1348
syryuauros <sy.ryu@aurostech.com>
2024-01-10 09:45:17 +0900

    [working-webUI-practice-digitMap] table select range improved

--- python3_10/projects/webUI/practice_template/digitMap/closedFunctions.js
+++ python3_10/projects/webUI/practice_template/digitMap/closedFunctions.js
@@ -20,19 +20,19 @@ function scientificRenderer(instance, td, row, col, prop, value, cellProperties)
 }
 
 function getDataFromSelectedRange(tableName) {
-  var selected = tableContent[tableName].getSelectedLast(); // Get the selected range coordinates [startRow, startCol, endRow, endCol]
+  var selected = tableContent[tableName].getSelected(); // Get the selected range coordinates [startRow, startCol, endRow, endCol]
   var selectedData = [];
 
-  for (var row = selected[0]; row <= selected[2]; row++) {
+  for (var row = selected[0][0]; row <= selected[0][2]; row++) {
     var rowData = [];
-
-    for (var col = selected[1]; col <= selected[3]; col++) {
-      rowData.push(tableContent[tableName].getDataAtCell(row, col));
+    for (var i = 0; i <= selected.length-1; i++) {
+      for (var col = selected[i][1]; col <= selected[i][3]; col++) {
+        rowData.push(tableContent[tableName].getDataAtCell(row, col));
+      }
     }
     selectedData.push(rowData);
   }
 
-  console.log('Selected data:', selectedData);
   return selectedData;
 }
 
