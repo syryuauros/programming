6947bc232ec94db42e83f05b6584212359eb74eb
syryuauros <sy.ryu@aurostech.com>
2024-02-16 17:27:43 +0900

    [working-ML-hybrid-thicknessDyna..]replace icons for scatter plot setting, minor modification

--- python3_10/projects/ML/hybrid/thicknessDynamicPrecision/html/007_plotly1(006).js
+++ python3_10/projects/ML/hybrid/thicknessDynamicPrecision/html/007_plotly1(006).js
@@ -5,6 +5,8 @@
 // heatMapPlot(tableName, plotName) {
 // heatMapPlotData(dataPlotTemp, plotName) {
 // scatterPlotData(dataPlotTemp, plotName) {
+// heatMapPlotSelectColumn(tableName, plotName, colNums) {
+// scatterPlotSelectColumn(tableName, plotName, colNums) {
 //
 // scatterPlotUpdate(plotId) {
 // scatterPlotUpdate2(plotId) {
@@ -27,6 +29,23 @@ let iconPallete = {
   'height': 512,
   'path': 'M512 256c0 .9 0 1.8 0 2.7c-.4 36.5-33.6 61.3-70.1 61.3H344c-26.5 0-48 21.5-48 48c0 3.4 .4 6.7 1 9.9c2.1 10.2 6.5 20 10.8 29.9c6.1 13.8 12.1 27.5 12.1 42c0 31.8-21.6 60.7-53.4 62c-3.5 .1-7 .2-10.6 .2C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm0-96a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM288 96a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm96 96a32 32 0 1 0 0-64 32 32 0 1 0 0 64z'
 };
+let iconRectangleList = {
+  'width': 576,
+  'height': 512,
+  'path': 'M0 96C0 60.7 28.7 32 64 32H512c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM128 288a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm32-128a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM128 384a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm96-248c-13.3 0-24 10.7-24 24s10.7 24 24 24H448c13.3 0 24-10.7 24-24s-10.7-24-24-24H224zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H448c13.3 0 24-10.7 24-24s-10.7-24-24-24H224zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H448c13.3 0 24-10.7 24-24s-10.7-24-24-24H224z'
+};
+let iconArrowUpDownLeftRight = {
+  'width': 512,
+  'height': 512,
+  'path': 'M278.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l9.4-9.4V224H109.3l9.4-9.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-9.4-9.4H224V402.7l-9.4-9.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-9.4 9.4V288H402.7l-9.4 9.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l9.4 9.4H288V109.3l9.4 9.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-64-64z'
+};
+let iconSliders = {
+  'width': 512,
+  'height': 512,
+  'path': 'M0 416c0 17.7 14.3 32 32 32l54.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 448c17.7 0 32-14.3 32-32s-14.3-32-32-32l-246.7 0c-12.3-28.3-40.5-48-73.3-48s-61 19.7-73.3 48L32 384c-17.7 0-32 14.3-32 32zm128 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM320 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32-80c-32.8 0-61 19.7-73.3 48L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l246.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48l54.7 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-54.7 0c-12.3-28.3-40.5-48-73.3-48zM192 128a32 32 0 1 1 0-64 32 32 0 1 1 0 64zm73.3-64C253 35.7 224.8 16 192 16s-61 19.7-73.3 48L32 64C14.3 64 0 78.3 0 96s14.3 32 32 32l86.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 128c17.7 0 32-14.3 32-32s-14.3-32-32-32L265.3 64z'
+};
+
+
 //download svg file from font-awesome, cat [path]/file.svg
 
 /////////////////////////////////////////////// configs ///////////////////////////////////////////////////////
@@ -55,7 +74,7 @@ const configPlotScatter = {
   {
     plotId: defaultPlotId,
     name: 'legend on/off',
-    icon: icon1,
+    icon: iconRectangleList,
     click: function() {
       showlegendMark = ! plot1.layout.showlegend;
       Plotly.relayout(this.plotId, { showlegend: showlegendMark, });
@@ -64,7 +83,7 @@ const configPlotScatter = {
   {
     plotId: defaultPlotId,
     name: 'axis setup',
-    icon: icon1,
+    icon: iconArrowUpDownLeftRight,
     click: function() {
       openPopUp(this.plotId, popupScatter1, 495, 750);
     },
@@ -72,7 +91,7 @@ const configPlotScatter = {
   {
     plotId: defaultPlotId,
     name: 'user setup',
-    icon: icon1,
+    icon: iconSliders,
     click: function() {
       //generateSelectBoxes(plot1.data.length, innerHtml1 );
       // generateElems('selectContainer', 'input', 4, innerHtml1);
@@ -165,6 +184,56 @@ function scatterPlotData(dataPlotTemp, headerTemp, plotName) {
     configs.modeBarButtonsToAdd[i].plotId = plotName;
   }
   Plotly.newPlot(plotName, dataTemp, layoutScatter, configs);
+}
+
+function heatMapPlotSelectColumn(tableName, plotName, colNums) {
+  dataPlotTemp = tableContent[tableName].getData();
+  var zValuesTemp = dataPlotTemp.map(function(row) {
+    return colNums.map(function(col) {
+      return row[col];
+    });
+  });
+
+  var dataTemp = [{
+    z: zValuesTemp,
+    type: 'heatmap',
+    colorscale: 'Hot', // Choose your desired color scale
+    zmin: 95,
+    zmax: 105,
+  }];
+
+  Plotly.newPlot(plotName, dataTemp, layoutHeatMap, configPlotHeatMap);
+}
+
+function scatterPlotSelectColumn(tableName, plotName, colNums) {
+  dataPlotTemp = tableContent[tableName].getData();
+  var trace = {
+    x: getColumn(dataPlotTemp,colNums[0]),
+    mode: 'markers+lines',
+    marker: {
+      symbol: 'circle', // Set the marker type here ('circle', 'square', 'diamond', 'cross', 'x', 'triangle-up', 'triangle-down', 'triangle-left', 'triangle-right', 'pentagon', 'hexagon', 'hexagram', 'star', etc.)
+      size: 7,
+    },
+    line: {
+      dash: 'dash', // Set the dash type here ('solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot')
+      width: 0,
+    },
+    type: 'scatter',
+    colorscale: 'Viridis' // Choose your desired color scale
+  };
+
+  dataTemp = [];
+
+  for (i = 1; i <= colNums.length-1; i++) {
+    traceTemp = Object.assign({}, trace);
+    traceTemp.y = getColumn(dataPlotTemp,colNums[i]);
+    traceTemp.z = i;
+    traceTemp.marker = { symbol: markerSymbol[i-1], size: markerSize[i-1], }
+    traceTemp.line = { dash: dashType[i-1], width: lineWidth[i-1], };
+    dataTemp.push(traceTemp);
+  }
+
+  Plotly.newPlot(plotName, dataTemp, layoutScatter, configPlotScatter);
 }
 
 function scatterPlotUpdate(plotId) {
