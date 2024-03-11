cfb9123f553df80b958732413305b9de86928691
syryuauros <sy.ryu@aurostech.com>
2024-03-11 15:54:32 +0900

    [working-ML-hybrid-thicknessDyna..]Data Base UI(easyui tree) basic functions arrange2

--- python3_10/projects/ML/hybrid/thicknessDynamicPrecision/htmlTree/script.js
+++ python3_10/projects/ML/hybrid/thicknessDynamicPrecision/htmlTree/script.js
@@ -1,5 +1,47 @@
+// var treeData = [];
+
+// $(document).ready(function() {
+// function initializeTree(treeID, treeData, treeContextMenu='treeContextMenu') { // treeID: str, treeData: var, treeContextMenu: str
+// var pathID;
+// function initializeContextMenu(treeID, treeData, treeContextMenu='treeContextMenu') {
+// function refreshEventHandlers(treeID, treeData) {
+//
+// function tree_findNodeByPath(treeData, path) {
+// function tree_findEmptyIndex(treeData, path) {
+// function tree_addChildAtPath(treeData, targetPath, childText, isFolder = true) {
+// function tree_addFolder(treeData, currentPath) {
+// function tree_addFile(treeData, currentPath) {
+// function tree_removeNode(treeID, treeData, selectedNode) {
+//
+// function openPopUp(popUpId, top, left) {
+// function closePopUp(popUpId) {
+//
+// function arr_deepCopy(arr) {
+// function arr_getRange(start, end) {
+// function arr_removeElementFromArray(arr, elem) {
+// function listObj_findObjectWithKeyValue(listOfObjects, key, value) {
+
+
 var treeData = [];
-treeData.push({"text": "DataBase", "path": [0]});
+treeData.push({"text": "DataBase", "path": [0], "state": "closed", "isFolder": true});
+
+// tree_addChildAtPath(treeData, [0, 0], "a");
+// tree_addChildAtPath(treeData, [0, 0, 0], "a1");
+// tree_addChildAtPath(treeData, [0, 0, 1], "a2");
+// tree_addChildAtPath(treeData, [0, 0, 1, 0], "a21");
+// // tree_addChildAtPath(treeData, [0, 0, 2], "a3");
+// tree_addChildAtPath(treeData, [0, 0, 3], "a4");
+// tree_addChildAtPath(treeData, [0, 0, 3, 0], "a41");
+// // tree_addChildAtPath(treeData, [0, 0, 3, 1], "a42");
+// tree_addChildAtPath(treeData, [0, 0, 4], "a5");
+// tree_addChildAtPath(treeData, [0, 1], "b");
+// tree_addChildAtPath(treeData, [0, 1, 0], "b1");
+// tree_addChildAtPath(treeData, [0, 1, 1], "b2");
+// tree_addChildAtPath(treeData, [0, 2], "c");
+// tree_addChildAtPath(treeData, [0, 2, 0], "c1");
+// tree_addChildAtPath(treeData, [0, 2, 1], "c2");
+// tree_addChildAtPath(treeData, [0, 3], "d");
+
 
 $(document).ready(function() {
   initializeTree('treeContainer', treeData);
@@ -38,19 +80,19 @@ function initializeTree(treeID, treeData, treeContextMenu='treeContextMenu') { /
 
     onAfterEdit: function(node) {
       var selectedNode = treeUI.tree('getSelected');
-      var currentNodeAtData = tree_findCurrentNode(treeData, selectedNode.path);
+      var currentNodeAtData = tree_findNodeByPath(treeData, selectedNode.path);
       currentNodeAtData.text = selectedNode.text;
       console.log(currentNodeAtData);
     },
 
     onExpand: function(node) {
       console.log("expending", node.path);
-      var currentNodeAtData = tree_findCurrentNode(treeData, node.path);
+      var currentNodeAtData = tree_findNodeByPath(treeData, node.path);
       currentNodeAtData.state = "open";
     },
     onCollapse: function(node) {
       console.log("collapsed", node.path);
-      var currentNodeAtData = tree_findCurrentNode(treeData, node.path);
+      var currentNodeAtData = tree_findNodeByPath(treeData, node.path);
       currentNodeAtData.state = "closed";
     },
   });
@@ -77,6 +119,7 @@ function initializeContextMenu(treeID, treeData, treeContextMenu='treeContextMen
       } else if (item.iconCls === 'icon-remove') {
         if (selectedNode) {
           tree_removeNode(treeID, treeData, selectedNode );
+          refreshEventHandlers('treeContainer', treeData);
         }
       } else if (item.iconCls === 'icon-edit') {
         if (selectedNode) {
@@ -101,55 +144,71 @@ function refreshEventHandlers(treeID, treeData) {
 
 
 
-
-
-
-
-
-function tree_findCurrentNode(treeData, path) {
-  var currentNode = treeData;
-  for (var i = 0; i < path.length - 1; i++)
-  {
-    if (!currentNode[path[i]].children) { currentNode[path[i]].children = []; }
-    currentNode = currentNode[path[i]].children;
+function tree_findNodeByPath(treeData, path) {
+  var currentNode = treeData[0];
+  var newPath = [0];
+  // console.log(path, ',' , newPath);
+  for (var i = 1; i < path.length; i++) {
+    currentNodeSiblings = currentNode.children;
+    // console.log('currentNodeSiblings: ', i, 'th ', currentNodeSiblings);
+    newPath.push(path[i]);
+    // console.log('newPath: ', newPath);
+    currentNode = listObj_findObjectWithKeyValue(currentNodeSiblings, 'path', newPath);
+    // console.log('pickedNode: ', i, 'th ', currentNode, 'path: ', path);
   }
-  return currentNode[path[path.length - 1]];
+  return currentNode;
 }
 
-function tree_findNumChildren(treeData, path) {
-  var currentNode = treeData;
-  for (var i = 0; i < path.length; i++)
-  {
-    if (!currentNode[path[i]].children) { currentNode[path[i]].children = []; }
-    currentNode = currentNode[path[i]].children;
+function tree_findEmptyIndex(treeData, path) {
+  let emptyIndex = 0;
+  let currentNode = tree_findNodeByPath(treeData, path);
+  let numChildren = 0;
+  if (!currentNode.children) {
+  } else {
+    numChildren = currentNode.children.length;
   }
-  return currentNode.length;
-}
-
-function tree_addChildAtPath(treeData, path, childText, isFolder = true) {
-  var currentNode = treeData;
-  for (var i = 0; i < path.length - 1; i++)
-  {
-    if (!currentNode[path[i]].children) { currentNode[path[i]].children = []; }
-    currentNode = currentNode[path[i]].children;
+  let indicies = arr_getRange(0, numChildren);
+  if (numChildren != 0) {
+    for(let i = 0; i < numChildren; i++) {
+      let pathInfo = arr_deepCopy(currentNode.children[i].path);
+      let index = pathInfo.pop();
+      indicies = arr_removeElementFromArray(indicies, index);
+      console.log("targetIndex: ", indicies, "index: ", index);
+    }
+  if (indicies == undefined) { emptyIndex = numChildren; }
+    else { emptyIndex = indicies[0]; }
   }
-  if (isFolder) { currentNode.push({ "text": childText, "path": path, "state": "closed"}); }
-  else { currentNode.push({ "text": childText, "path": path}); }
+  console.log("emptyIndex: ", emptyIndex);
+  return emptyIndex;
 }
 
+function tree_addChildAtPath(treeData, targetPath, childText, isFolder = true) {
+  let newPath = arr_deepCopy(targetPath);
+  let targetIndex = newPath.pop();
+  let currentNode = tree_findNodeByPath(treeData, newPath);
+  if (!currentNode.children) { currentNode.children = []; };
+  console.log('currentNode.children: ', currentNode.children);
 
-function tree_addFolder(treeData, path) {
-  numChildren = tree_findNumChildren(treeData, path);
-  var newPath = arr_deepCopy(path);
-  newPath.push(numChildren);
-  tree_addChildAtPath(treeData, newPath, "NewFolder", true);
+  if (isFolder) { currentNode.children.push({ "text": childText, "path": targetPath, "state": "closed", "isFolder": true}); }
+  else { currentNode.children.push({ "text": childText, "path": targetPath, "isFolder": false}); }
 }
 
-function tree_addFile(treeData, path) {
-  numChildren = tree_findNumChildren(treeData, path);
-  var newPath = arr_deepCopy(path);
-  newPath.push(numChildren);
-  tree_addChildAtPath(treeData, newPath, "NewFile", false);
+function tree_addFolder(treeData, currentPath) {
+  var currentNodeAtData = tree_findNodeByPath(treeData, currentPath);
+  if (currentNodeAtData.isFolder == true) {
+    let targetIndex = tree_findEmptyIndex(treeData, currentPath);
+    let newPath = [ ...currentPath, targetIndex];
+    tree_addChildAtPath(treeData, newPath, 'NewFolder', true);
+  }
+}
+
+function tree_addFile(treeData, currentPath) {
+  var currentNodeAtData = tree_findNodeByPath(treeData, currentPath);
+  if (currentNodeAtData.isFolder == true) {
+    let targetIndex = tree_findEmptyIndex(treeData, currentPath);
+    let newPath = [ ...currentPath, targetIndex];
+    tree_addChildAtPath(treeData, newPath, 'NewFile', false);
+  }
 }
 
 function tree_removeNode(treeID, treeData, selectedNode) {
@@ -158,30 +217,23 @@ function tree_removeNode(treeID, treeData, selectedNode) {
   let targetPathOrigin = arr_deepCopy(selectedNode.path);
   let targetPath = arr_deepCopy(selectedNode.path);
   let numChildToBeDel = targetPath.pop();
-  let parentNodeAtData = tree_findCurrentNode(treeData, targetPath);
+  let parentNodeAtData = tree_findNodeByPath(treeData, targetPath);
   let numChildren = parentNodeAtData.children.length;
-  console.log(parentNodeAtData);
-  console.log(targetPathOrigin);
-  console.log(targetPath);
-  console.log(numChildToBeDel);
-  console.log(numChildren);
+  // console.log(parentNodeAtData);
+  // console.log(targetPathOrigin);
+  // console.log(targetPath);
+  // console.log(numChildToBeDel);
+  // console.log(numChildren);
   parentNodeAtData.children.splice(numChildToBeDel, 1);
-  for (var i = 0; i < numChildren - 1; i++) {
-    let newTargetPath = [...targetPath, i];
-    console.log('newTargetPath: ' + newTargetPath);
-    parentNodeAtData.children[i].path = newTargetPath;
-    console.log(i + 'th path ' + parentNodeAtData.children[i].path);
-  }
+  // for (var i = 0; i < numChildren - 1; i++) {
+  //   let newTargetPath = [...targetPath, i];
+  //   console.log('newTargetPath: ' + newTargetPath);
+  //   parentNodeAtData.children[i].path = newTargetPath;
+  //   console.log(i + 'th path ' + parentNodeAtData.children[i].path);
+  // }
   console.log(parentNodeAtData);
-  refreshEventHandlers('treeContainer', treeData);
+  // refreshEventHandlers('treeContainer', treeData);
 }
-
-
-
-
-
-
-
 
 
 function openPopUp(popUpId, top, left) {
@@ -199,3 +251,50 @@ function closePopUp(popUpId) {
 function arr_deepCopy(arr) {
   return [...arr];
 }
+
+function arr_getRange(start, end) {
+  let result = [];
+  for (let i = start; i <= end; i++) {
+      result.push(i);
+  }
+  return result;
+}
+
+function arr_removeElementFromArray(arr, elem) {
+    return arr.filter(function(value) {
+        return value !== elem;
+    });
+}
+
+function listObj_findObjectWithKeyValue(listOfObjects, key, value) {
+  return listOfObjects.find(function(obj) {
+    if (Array.isArray(obj[key]) && Array.isArray(value)) {
+      return JSON.stringify(obj[key]) === JSON.stringify(value);
+    } else {
+      return obj[key] === value;
+    }
+  });
+}
+
+
+
+
+// function tree_findCurrentNode(treeData, path) {
+//   var currentNode = treeData;
+//   for (var i = 0; i < path.length - 1; i++)
+//   {
+//     if (!currentNode[path[i]].children) { currentNode[path[i]].children = []; }
+//     currentNode = currentNode[path[i]].children;
+//   }
+//   return currentNode[path[path.length - 1]];
+// }
+
+// function tree_findNumChildren(treeData, path) {
+//   var currentNode = treeData;
+//   for (var i = 0; i < path.length; i++)
+//   {
+//     if (!currentNode[path[i]].children) { currentNode[path[i]].children = []; }
+//     currentNode = currentNode[path[i]].children;
+//   }
+//   return currentNode.length;
+// }
