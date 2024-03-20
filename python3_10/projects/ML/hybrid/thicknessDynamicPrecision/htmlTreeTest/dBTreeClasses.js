effb513ed7e6623f05951547bb4ff97060e44455
syryuauros <sy.ryu@aurostech.com>
2024-03-20 10:07:48 +0900

    [working-ML-hybrid-thicknessDyna..]DataBase, file Name arranged, htmlTreeTest

--- python3_10/projects/ML/hybrid/thicknessDynamicPrecision/htmlTreeTest/dBTreeClasses.js
+++ python3_10/projects/ML/hybrid/thicknessDynamicPrecision/htmlTreeTest/dBTreeClasses.js
@@ -440,20 +440,3 @@ function openPopUp(popUpId, top, left) {
 function closePopUp(popUpId) {
   popUpId.style.display = 'none';
 }
-
-var dataArrayToSave = [
-  ['John2', 30],
-  ['Alice2', 25],
-  ['Bob2', 35]
-];
-
-const arrF = new arrFunctions();
-const strF = new strFunctions();
-const looF = new listObjFunctions();
-// const treeF = new treeFunctions();
-const dBTree1 = new dBTree('dBTreeTest');
-
-$(document).ready(async function () {
-  dBTree1.initializeTree();
-  await dBTree1.treeF.fetchDB();
-})
